9310a257f594185452fe50fc14d83a81bd3dc057
Malleshwari-c <148347071+Malleshwari-c@users.noreply.github.com>
2026-09-11 11:27:45 +0530

    Update Assessmentday_1_1.java

--- Assessmentday_1_1.java
+++ Assessmentday_1_1.java
@@ -22,24 +22,27 @@ import org.openqa.selenium.io.FileHandler;
 public class ScreenshotTest 
 {
 	public static void main(String[] args) throws Exception {
-        WebDriver driver = new ChromeDriver();
+        //launch browser
+		WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//naviagte to url
         driver.get("https://www.saucedemo.com/");
-
+		//login
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
-        
+        //scroll down
         JavascriptExecutor js=(JavascriptExecutor)driver;
 		js.executeScript("window.scrollTo(0,document.body.scrollHeight)");
-		
+		//take screenshot
 		TakesScreenshot tks = (TakesScreenshot) driver;
-		
+		//save to the file
 		File src = tks.getScreenshotAs(OutputType.FILE);
 		File dest = new File("./screenshots/products-page.png");
 		FileHandler.copy(src, dest);
 		Thread.sleep(3000);
-		
+		//close
 		driver.quit();
 }
 }
