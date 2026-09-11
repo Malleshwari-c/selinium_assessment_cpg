bcdef697411ee47b57b5f53c86bbebd7f373a5bf
Malleshwari-c <148347071+Malleshwari-c@users.noreply.github.com>
2026-09-11 11:24:36 +0530

    Update AssessmentDay_3.java

--- AssessmentDay_3.java
+++ AssessmentDay_3.java
@@ -47,3 +47,49 @@ public class day_3_1 {
 	}
 
 }
+
+
+//Assessment 3 test case 1
+
+package assessment;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+
+public class day_3_2 {
+	public static void main(String[] args) throws InterruptedException {
+		//launch webDriver
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		//navigate to url
+		driver.get("https://demoapps.qspiders.com/ui/datePick?sublist=0");
+		//select the date picker
+		driver.findElement(By.xpath("//section[.='Date & Time Picker']")).click();
+		driver.findElement(By.xpath("//section[.='Date Picker']")).click();
+		//select the calender icon
+		WebElement ref=driver.findElement(By.cssSelector("[class='react-datepicker-wrapper']"));
+		driver.findElement(RelativeLocator.with(By.tagName("svg")).toRightOf(ref)).click();
+		driver.findElement(By.xpath("//button[@class='react-datepicker__navigation react-datepicker__navigation--next']")).click();
+		//select the date
+		driver.findElement(By.xpath("//div[.='8']")).click();
+		//verification
+		WebElement verify=driver.findElement(By.xpath("//input[@placeholder='Select A Date']"));
+		System.out.println(verify.getAttribute("value"));
+		if(verify.getAttribute("value").contains("8/10/2026")) {
+			System.out.println("selected date is displayed");
+		}else {
+			System.out.println("selected date is not displayed");
+		}
+		Thread.sleep(2000);
+		//close the browser
+		driver.quit();
+	}
+
+}
+
