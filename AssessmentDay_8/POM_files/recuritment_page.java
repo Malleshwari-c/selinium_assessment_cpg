4e964a00f291ffd0d6cc5d8d401f25bce943eda1
Malleshwari-c <148347071+Malleshwari-c@users.noreply.github.com>
2026-09-17 12:01:11 +0530

    Update recuritment_page.java

--- AssessmentDay_8/POM_files/recuritment_page.java
+++ AssessmentDay_8/POM_files/recuritment_page.java
@@ -6,18 +6,21 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class recuritment_page {
+	//create driver as global variable
 	WebDriver driver;
-	
+	//constructor call and pass driver as argument
 	public recuritment_page(WebDriver driver) {
 		this.driver=driver;
+	    //initiate all the variables using PageFactory
 		PageFactory.initElements(driver,this);
 		}
-	
+	//find recurment button using css selector
 	@FindBy(css="[href='/web/index.php/recruitment/viewRecruitmentModule']")
 	private WebElement recur;
 	
-
+	//create recu method 
 	public void recu() {
+		//click on the button
 		recur.click();
 	}
 
