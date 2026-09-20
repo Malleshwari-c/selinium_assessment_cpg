56881eeb2bf723b1cb2551ac075df7fdd03da87c
Malleshwari-c <148347071+Malleshwari-c@users.noreply.github.com>
2026-09-20 15:00:26 +0530

    Update addJobVacancy_page.java

--- AssessmentDay_11/TestCase1/POMfiles/addJobVacancy_page.java
+++ AssessmentDay_11/TestCase1/POMfiles/addJobVacancy_page.java
@@ -1,6 +1,10 @@
 package Assessment12;
 
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,16 +12,18 @@ import org.openqa.selenium.support.PageFactory;
 
 public class addJobVacancy12 {
 WebDriver driver;
+Robot r;
 	
-	public addJobVacancy12(WebDriver driver) {
+	public addJobVacancy12(WebDriver driver) throws AWTException {
 		this.driver=driver;
+		r=new Robot();
 		PageFactory.initElements(driver,this);
 	}
 	
 	@FindBy(xpath="(//input[@class='oxd-input oxd-input--active'])[2]")
 	private WebElement vacancyName;
 	
-	@FindBy(xpath="//div[@role='listbox' and normalize-space()='\" + JT + \"']")
+	@FindBy(xpath="//div[text()='-- Select --']")
 	private WebElement selectClick;
 	
 	@FindBy(xpath="//div[@class='oxd-select-text-input']")
@@ -32,11 +38,19 @@ WebDriver driver;
 	@FindBy(css="[type='submit']")
 	private WebElement saveClick;
 
-	public void vacanyJob(String VN,String JT,String des,String HM,String Pos) {
+	public void vacanyJob(String VN,String JT,String des,String HM,String Pos) throws InterruptedException {
 		System.out.println("filling started");
 		vacancyName.sendKeys(VN);
 		System.out.println("vaname entered");
 		selectClick.click();
+        Thread.sleep(2000);
+        r.keyPress(KeyEvent.VK_DOWN);
+        r.keyRelease(KeyEvent.VK_DOWN);
+        r.keyPress(KeyEvent.VK_DOWN);
+        r.keyRelease(KeyEvent.VK_DOWN);
+        r.keyPress(KeyEvent.VK_ENTER);
+        r.keyRelease(KeyEvent.VK_ENTER);
+        Thread.sleep(2000);
 		System.out.println("click selection");
 		description.sendKeys(des);
 		System.out.println("descript entered");
