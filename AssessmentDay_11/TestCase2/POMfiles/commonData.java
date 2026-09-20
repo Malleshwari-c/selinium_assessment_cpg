26668f15f61d8256511772ac164732035f9c0f35
Malleshwari-c <148347071+Malleshwari-c@users.noreply.github.com>
2026-09-20 23:27:43 +0530

    Create commonData class for configuration management
    Added commonData class to manage browser and user credentials.

---
+++ AssessmentDay_11/TestCase2/POMfiles/commonData.java
@@ -0,0 +1,29 @@
+package Assess12_testcase2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class commonData {
+	public String BROWSER;
+	public String URL;
+	public String USERNAME,PASSWORD;
+	
+	public void bowser() throws IOException {
+		FileInputStream fis=new FileInputStream("./src/test/resources/DDT1/comData.properties");
+		Properties pro=new Properties();
+		pro.load(fis);
+		BROWSER=pro.getProperty("browser");
+		URL=pro.getProperty("url");
+		USERNAME=pro.getProperty("username");
+		PASSWORD=pro.getProperty("password");
+	}
+
+}
